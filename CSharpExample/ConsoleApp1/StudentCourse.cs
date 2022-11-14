a71b91d4986297608f0f7d8688d40c22f6b66333
Arslan Tek <official.arslan@outlook.com>
2022-11-14 09:09:41 +0300

    First Commit

---
+++ CSharpExample/ConsoleApp1/StudentCourse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class StudentCourse
+    {
+        public int Id { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int TeacherCourseId { get; set; }
+    }
+}
